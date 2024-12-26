e33f0344eb3372eba97b205093931c26cad76933
john <john@doe>
2024-12-26 18:29:33 +0900

    db::avatar,channel

--- src/index.js
+++ src/index.js
@@ -42,20 +42,138 @@ const mockGames = {
     { id: 2, title: '리틀킹', image: '/images/TA002.png' },
     { id: 3, title: '신전', image: '/images/TA001.png' },
     { id: 4, title: '슈퍼보난자', image: '/images/TA002.png' }
+  ],
+  avatars: [
+    [
+      {
+        "price": 10000,
+        "name": "Avatar #1",
+        "image": "/images/avatar_0088.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #2",
+        "image": "/images/avatar_0335.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #3",
+        "image": "/images/avatar_0445.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #4",
+        "image": "/images/avatar_1622.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #5",
+        "image": "/images/avatar_1865.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #6",
+        "image": "/images/avatar_2498.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #7",
+        "image": "/images/avatar_3063.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #8",
+        "image": "/images/avatar_3164.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #9",
+        "image": "/images/avatar_3204.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #10",
+        "image": "/images/avatar_3212.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #11",
+        "image": "/images/avatar_3397.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #12",
+        "image": "/images/avatar_4126.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #13",
+        "image": "/images/avatar_5998.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #14",
+        "image": "/images/avatar_6685.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #15",
+        "image": "/images/avatar_7043.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #16",
+        "image": "/images/avatar_7384.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #17",
+        "image": "/images/avatar_7622.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #18",
+        "image": "/images/avatar_7627.jpg"
+      },
+      {
+        "price": 10000,
+        "name": "Avatar #19",
+        "image": "/images/avatar_7873.jpg"
+      }
+    ]
   ]
+
 };
 
 // Routes
-app.get('/', (req, res) => {
-  res.render('index', { title: '아바타 상점' });
+app.get('/', async (req, res) => {
+  let avatars = mockGames.avatars;
+  try {
+    avatars = await executeQuery("select price, title as name, image as image from avatar;", []);
+    console.log(JSON.stringify(avatars, null, 2));
+  } catch (e) {
+  }
+  res.render('index', { title: '아바타 상점' , avatars});
 });
 
-app.get('/avatar-shop', (req, res) => {
-  res.render('avatar-shop', { title: '아바타 상점' });
+app.get('/avatar-shop', async (req, res) => {
+  let avatars = mockGames.avatars;
+  try {
+    avatars = await executeQuery("select price, title as name, image as image from avatar;", []);
+    console.log(JSON.stringify(avatars, null, 2));
+  } catch (e) {
+  }
+  res.render('avatar-shop', { title: '아바타 상점' , avatars});
 });
 
-app.get('/holdem', (req, res) => {
-  res.render('holdem', { title: '홀덤' });
+app.get('/holdem', async (req, res) => {
+  let channels = mockGames.channel;
+  try {
+    channels = await executeQuery("select channelName as name, description as requirement, minimum from pokerChannel;", []);
+    console.log(JSON.stringify(channels, null, 2));
+  } catch (e) {
+  }
+  res.render('holdem', { title: '홀덤', channels });
 });
 
 app.get('/slot', async (req, res) => {
