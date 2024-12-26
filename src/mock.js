aae79d3526606813ed5391dcf958fc5672d3da55
john <john@doe>
2024-12-26 18:52:31 +0900

    mock

---
+++ src/mock.js
@@ -0,0 +1,159 @@
+
+export const mockGames = {
+    popular: [
+        {
+            "title": "Book",
+            "image": "/images/TA001.png"
+        },
+        {
+            "title": "King Arthur",
+            "image": "/images/TA002.png"
+        }
+    ]
+    ,
+    all: [
+        { id: 1, title: '아낙수나문', image: '/images/TA001.png' },
+        { id: 2, title: '리틀킹', image: '/images/TA002.png' },
+        { id: 3, title: '신전', image: '/images/TA001.png' },
+        { id: 4, title: '슈퍼보난자', image: '/images/TA002.png' }
+    ],
+    avatars: [
+        [
+            {
+                "price": 10000,
+                "name": "Avatar #1",
+                "image": "/images/avatar_0088.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #2",
+                "image": "/images/avatar_0335.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #3",
+                "image": "/images/avatar_0445.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #4",
+                "image": "/images/avatar_1622.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #5",
+                "image": "/images/avatar_1865.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #6",
+                "image": "/images/avatar_2498.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #7",
+                "image": "/images/avatar_3063.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #8",
+                "image": "/images/avatar_3164.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #9",
+                "image": "/images/avatar_3204.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #10",
+                "image": "/images/avatar_3212.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #11",
+                "image": "/images/avatar_3397.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #12",
+                "image": "/images/avatar_4126.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #13",
+                "image": "/images/avatar_5998.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #14",
+                "image": "/images/avatar_6685.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #15",
+                "image": "/images/avatar_7043.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #16",
+                "image": "/images/avatar_7384.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #17",
+                "image": "/images/avatar_7622.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #18",
+                "image": "/images/avatar_7627.jpg"
+            },
+            {
+                "price": 10000,
+                "name": "Avatar #19",
+                "image": "/images/avatar_7873.jpg"
+            }
+        ]
+    ],
+    channels: [
+        {
+            "name": "자유",
+            "requirement": "제한없음",
+            "minimum": 0
+        },
+        {
+            "name": "아마추어",
+            "requirement": "3억 이상",
+            "minimum": 300000000
+        },
+        {
+            "name": "프로",
+            "requirement": "6억 이상",
+            "minimum": 600000000
+        },
+        {
+            "name": "챔피언",
+            "requirement": "10억 이상",
+            "minimum": 1000000000
+        }
+    ],
+    memberships: [
+        {
+            "id": 1,
+            "earnedExp": 5,
+            "bonusMoney": 30000000,
+            "monthlyFee": 3000,
+            "imageUrl": "/images/membership_premium.png",
+            "membershipName": "프리미엄"
+        },
+        {
+            "id": 2,
+            "earnedExp": 7,
+            "bonusMoney": 30000000,
+            "monthlyFee": 5000,
+            "imageUrl": "/images/membership_vip.png",
+            "membershipName": "VIP"
+        }
+    ]
+};
